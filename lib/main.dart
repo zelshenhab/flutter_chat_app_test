1c6fa1068d7c28abcd150b16ee83bfc43c5677cd
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2025-02-27 17:59:17 +0300

    add chats screen

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,6 @@ class Message extends HiveObject {
   });
 }
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
@@ -42,4 +40,78 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(home: ChatsScreen());
   }
+}
+
+final List<ChatUser> users = [
+  ChatUser(name: 'Viktor Vlasov', avatar: 'VV', chatBoxName: 'chat_viktor'),
+  ChatUser(name: 'Sasha Alexeeva', avatar: 'SA', chatBoxName: 'chat_sasha'),
+  ChatUser(name: 'Pyotr Zharinov', avatar: 'PZ', chatBoxName: 'chat_pyotr'),
+  ChatUser(name: 'Alina Zhukova', avatar: 'AZ', chatBoxName: 'chat_alina'),
+];
+
+class ChatsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Chats',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          return FutureBuilder<Box<Message>>(
+            future: Hive.openBox<Message>(user.chatBoxName),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return CircularProgressIndicator();
+              final box = snapshot.data!;
+              final lastMessage = box.isNotEmpty ? box.values.last : null;
+
+              return ListTile(
+                leading: CircleAvatar(child: Text(user.avatar)),
+                title: Text(
+                  user.name,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  lastMessage != null
+                      ? (lastMessage.type == 'text'
+                          ? lastMessage.content
+                          : '[Image]')
+                      : 'No messages yet',
+                ),
+                trailing: Text(
+                  lastMessage != null ? formatTime(lastMessage.timestamp) : '',
+                ),
+                onTap:
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => ChatScreen(user: user)),
+                    ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  String formatTime(DateTime time) {
+    return "${time.hour}:${time.minute}";
+  }
+}
+
+class ChatUser {
+  final String name;
+  final String avatar;
+  final String chatBoxName;
+
+  ChatUser({
+    required this.name,
+    required this.avatar,
+    required this.chatBoxName,
+  });
 }
